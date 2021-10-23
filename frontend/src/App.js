fd421b0fc0075836c3331c77e64e7dda1f956def
OlirEswar <92946878+OlirEswar@users.noreply.github.com>
2021-10-23 00:53:19 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,39 @@ import axios from 'axios';
 //function App() {
 class App extends Component {
 
+  state = {
+    posts: [],
+  }
+
+  componentDidMount = () => {
+    this.getPost();
+  };
+
+  getPost = () => {
+    axios.get('http://localhost:5000/problem/')
+    .then((response) => {
+      const data = response.data;
+      this.setState({ posts: data });
+      console.log('Data has been received!!');
+    })
+    .catch(() => {
+      alert('Data has not been received!!'); //change to error catch
+    });
+  }
+
   onSubmit = (fields) => {
     console.log("App comp got: ", fields);
+  };
+
+  displayPosts = (posts) => {
+    if (!posts.length) return null;
+
+    return posts.map((post, index) => (
+      <div key={index}>
+        <h3>{post.subject}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
   };
 
   render() {
@@ -18,6 +49,9 @@ class App extends Component {
       <div className="App">
         <Title />
         <Form onSubmit={fields => this.onSubmit(fields)} />
+        <div className="posts">
+          {this.displayPosts(this.state.posts)}
+        </div>
       </div>
     );
   }
