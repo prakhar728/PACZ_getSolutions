ea080c13f403314ddde9ba92ce91809fe3e91b8c
OlirEswar <92946878+OlirEswar@users.noreply.github.com>
2021-10-22 23:56:02 -0400

    Update Form.js

--- frontend/src/Form.js
+++ frontend/src/Form.js
@@ -1,21 +1,26 @@
 import React from 'react';
-
+import axios from 'axios';
 export default class Form extends React.Component {
     state = {
         username: '',
         subject: '',
         body: '',
+        anonymous: false,
     }
 
-    change = (e) => {
+    /*change = (e) => {
         this.setState({
             [e.target.name]: e.target.value
         });
-    };
+    };*/
 
     onSubmit = (e) => {
         e.preventDefault();
         this.props.onSubmit(this.state)
+        axios.get('http://localhost:5000/problem/')
+        .then(resp=>console.log(resp))
+        .catch(err=>
+            console.log(err))
         this.setState({
             username: '',
             subject: '',
@@ -30,23 +35,31 @@ export default class Form extends React.Component {
                     name="username"
                     placeholder="username"
                     value={this.state.username} 
-                    onChange={e => this.change(e)}
-                    //onChange={e => this.setState({ username: e.target.value })}
+                    //onChange={e => this.change(e)}
+                    onChange={e => this.setState({ username: e.target.value })}
                 />
                 <br />
                 <input 
                     name="subject"
                     placeholder="subject"
                     value={this.state.subject} 
-                    onChange={e => this.change(e)}
+                    onChange={e => this.setState({ subject: e.target.value })}
                 />
                 <br />
                 <input 
                     name="body"
                     placeholder="body"
                     value={this.state.body} 
-                    onChange={e => this.change(e)}
+                    onChange={e => this.setState({ body: e.target.value })}
+
                 />
+                <br />
+                <input 
+                type="checkbox" 
+                id="Post Anonymously?"
+                onChange={e => this.setState({ anonymous: !(this.state.anonymous) })}
+                />
+                <label for="Post Anonymously">Post Anonymously</label>
                 <br />
 
                 <button onClick={e => this.onSubmit(e)}>Submit</button>
